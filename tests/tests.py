0222966be0427aa1643f1ac19307585181d410b4
Tayler Mulligan <mulligantayler@gmail.com>
2013-08-14 23:36:41 -0700

    simplified output redirection
    Replaced `logging_file` argument with `stream`
    stream accepts a string of a file's full path, or an object that
    implements `write` and `flush`

--- tests/tests.py
+++ tests/tests.py
@@ -1,89 +1,77 @@
 import simplelogger
-import sys
-import os
 import re
 
 
-log_file = "/Users/tayler/Documents/Programming/Python/simplelogger/tests/output"
+class MockStream(object):
+    def __init__(self):
+        self.output = []
+        self.buffer = ''
 
-def test():
-    assert True
+    def write(self,text):
+        self.buffer = text
+
+    def flush(self):
+        self.output.append(self.buffer)
 
 class TestBasics(object):
     def test_output(self):
-        global log_file
-        self.log_file = log_file
-        logger = simplelogger.Logger(log_file=self.log_file)
+        stream = MockStream()
+        logger = simplelogger.Logger(stream=stream)
         logger.info("test")
-        last = get_last()
-        last = last.split()
-        assert last[-1] == "test"
-        os.remove(self.log_file)
+        output = stream.output[-1].split()
+        assert output[-1] == "test"
 
 
 class TestLevels(object):
     def setup(self):
-        global log_file
-        self.log_file = log_file
-        self.l = simplelogger.Logger(log_file=self.log_file,threshold='debug')
+        self.stream = MockStream()
+        self.l = simplelogger.Logger(stream=self.stream)
         self.l.logging_format('{level}')
-
-    def teardown(self):
-        os.remove(self.log_file)
 
     def test_info(self):
         self.l.info("test")
-        assert get_last() == "INFO\n"
+        assert self.stream.output[-1] == "INFO\n"
 
     def test_debug(self):
         self.l.set_threshold("debug")
         self.l.debug("test")
-        print open(self.log_file).read()
-        assert get_last() == "DEBUG\n"
+        assert self.stream.output[-1] == "DEBUG\n"
 
     def test_warning(self):
         self.l.warning("test")
-        assert get_last() == "WARNING\n"
+        assert self.stream.output[-1] == "WARNING\n"
 
     def test_error(self):
         self.l.error("test")
-        assert get_last() == "ERROR\n"
+        print self.stream.output[-1]
+        assert self.stream.output[-1] == "ERROR\n"
 
     def test_critical(self):
         self.l.critical("test")
-        assert get_last() == "CRITICAL\n"
+        assert self.stream.output[-1] == "CRITICAL\n"
 
     def test_threshold(self):
         self.l.set_threshold("warning")
         self.l.info("test")
         self.l.warning("test")
 
-        output = open(self.log_file).readlines()
+        output = self.stream.output[-1]
 
-        assert output[1] != "INFO\n" and output[1] == "WARNING\n"
+        assert output != "INFO\n" and output == "WARNING\n"
 
 
 class TestFormat(object):
-    def teardown(self):
-        os.remove(self.log_file)
-
     def setup(self):
-        global log_file
-        self.log_file = log_file
-        self.l = simplelogger.Logger(log_file=self.log_file)
+        self.stream = MockStream()
+        self.l = simplelogger.Logger(stream=self.stream)
         self.l.logging_format('{datetime} {date} {time} {level} {text}')
 
     def test_all(self):
         self.l.info("test")
 
-        output = open("/Users/tayler/Documents/Programming/Python/simplelogger/tests/output").readlines()[-1]
+        output = self.stream.output[-1]
 
         if re.match('([0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}.[0-9]{6} ){2}\S* \S*', output):
             assert True
         else:
             assert False
-
-
-def get_last():
-    output = open("/Users/tayler/Documents/Programming/Python/simplelogger/tests/output", 'r').readlines()
-    return output[-1]
